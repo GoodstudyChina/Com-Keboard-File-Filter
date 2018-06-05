defb36b992091d5f28e4d2066388d66b59c2f1fd
xiaobo93 <guoxiaobo93@163.com>
2018-06-05 14:15:49 +0800

    x64 x32位程序正常运行
    解决x64位，获取进程全路径 蓝屏的问题

--- 驱动  小程序/阻止进程创建.c
+++ 驱动  小程序/阻止进程创建.c
@@ -1,70 +1,135 @@
-#include<ntddk.h>
-
-WCHAR* GetProcessImageFullPathByID(IN PHANDLE pID)
+#include<ntifs.h>
+NTSTATUS
+GetProcessImagePath(
+	OUT PUNICODE_STRING ProcessImagePath,
+	IN   HANDLE    dwProcessId
+)
 {
-	typedef NTSTATUS NTAPI NTQUERYINFORMATIONPROCESS(
-		_In_		HANDLE ProcessHandle,
-		_In_		PROCESSINFOCLASS ProcessInformationClass,
-		_Out_		PVOID ProcessInformation,
-		_In_		ULONG ProcessInformationLength,
-		_Out_opt_	PULONG ReturnLength
-	);
-	typedef NTQUERYINFORMATIONPROCESS FAR * LPNTQUERYINFORMATIONPROCESS;
-	UNICODE_STRING ProcessImageName;
-	NTSTATUS status = STATUS_SUCCESS;
+	NTSTATUS Status;
+	HANDLE  hProcess;
+	PEPROCESS pEprocess;
 	ULONG  returnedLength;
-	LPNTQUERYINFORMATIONPROCESS ZwQueryInformationProcess = NULL;
+	ULONG  bufferLength;
+	PVOID  buffer;
+	PUNICODE_STRING imageName;
 
-	HANDLE processHandle;
-	CLIENT_ID parentID;
-	OBJECT_ATTRIBUTES ObjectAttributes;
 
-	PAGED_CODE();
-	if (NULL == ZwQueryInformationProcess)
-	{
-		UNICODE_STRING routinName;
-		RtlInitUnicodeString(&routinName, L"ZwQueryInformationProcess");
-		ZwQueryInformationProcess = (LPNTQUERYINFORMATIONPROCESS)MmGetSystemRoutineAddress(&routinName);
-		if (NULL == ZwQueryInformationProcess)
-			KdPrint(("Cannot resolve ZwQueryInformationProcess\n"));
+	typedef NTSTATUS(*QUERY_INFO_PROCESS) (
+		__in HANDLE ProcessHandle,
+		__in PROCESSINFOCLASS ProcessInformationClass,
+		__out_bcount(ProcessInformationLength) PVOID ProcessInformation,
+		__in ULONG ProcessInformationLength,
+		__out_opt PULONG ReturnLength
+		);
+
+	QUERY_INFO_PROCESS ZwQueryInformationProcess =NULL;
+
+
+	PAGED_CODE();  // this eliminates the possibility of the IDLE Thread/Process   
+
+	if (NULL == ZwQueryInformationProcess) {
+
+		UNICODE_STRING routineName;
+
+		RtlInitUnicodeString(&routineName, L"ZwQueryInformationProcess");
+
+		ZwQueryInformationProcess =
+			(QUERY_INFO_PROCESS)MmGetSystemRoutineAddress(&routineName);
+
+		if (NULL == ZwQueryInformationProcess) {
+			DbgPrint("Cannot resolve ZwQueryInformationProcess/n");
+		}
 	}
 
-	InitializeObjectAttributes(&ObjectAttributes, 0, OBJ_CASE_INSENSITIVE | OBJ_KERNEL_HANDLE, 0, 0);
-	parentID.UniqueProcess = pID;
-	parentID.UniqueThread = 0;
+	Status = PsLookupProcessByProcessId((HANDLE)dwProcessId, &pEprocess);
+	if (!NT_SUCCESS(Status))
+		return Status;
 
-	status = ZwOpenProcess(&processHandle, PROCESS_ALL_ACCESS, &ObjectAttributes, &parentID);
+	Status = ObOpenObjectByPointer(pEprocess,           // Object   
+		OBJ_KERNEL_HANDLE,   // HandleAttributes   
+		NULL,                // PassedAccessState OPTIONAL   
+		GENERIC_READ,        // DesiredAccess   
+		*PsProcessType,      // ObjectType   
+		KernelMode,          // AccessMode   
+		&hProcess);
+	if (!NT_SUCCESS(Status))
+		return Status;
 
-	status = ZwQueryInformationProcess(processHandle,
+
+	//   
+	// Step one - get the size we need   
+	//   
+	Status = ZwQueryInformationProcess(hProcess,
 		ProcessImageFileName,
-		NULL,//buffer
-		0,  //buffer size
-		&returnedLength
-	);
-
-	//
-	// Is the passed-in buffer going to be big enough for us? 
-	// This function returns a single contguous buffer model...
-	//
-	ProcessImageName.Length = (USHORT)returnedLength;
-	ProcessImageName.MaximumLength = (USHORT)returnedLength;
-	ProcessImageName.Buffer = ExAllocatePoolWithTag(NonPagedPool,
-		ProcessImageName.Length,
-		'gxb');
-	//
-	// If we get here, the buffer IS going to be big enough for us, so
-	// let's allocate some storage.
-	//
-	status = ZwQueryInformationProcess(processHandle,
-		ProcessImageFileName,
-		ProcessImageName.Buffer,
-		ProcessImageName.Length,
+		NULL,  // buffer   
+		0,  // buffer size   
 		&returnedLength);
 
-	if (!NT_SUCCESS(status))
-		KdPrint(("ZwQueryInformationProcess error code %x ", status));
 
-	return ProcessImageName.Buffer;
+	if (STATUS_INFO_LENGTH_MISMATCH != Status) {
+		KdPrint(("ZwQueryInformationProcess don't get buffer length\r\n"));
+		return  Status;
+
+	}
+
+	//   
+	// Is the passed-in buffer going to be big enough for us?    
+	// This function returns a single contguous buffer model...   
+	//   
+	bufferLength = returnedLength - sizeof(UNICODE_STRING);
+
+	if (ProcessImagePath->MaximumLength < bufferLength) {
+
+		ProcessImagePath->Length = (USHORT)returnedLength;
+		ProcessImagePath->MaximumLength = (USHORT)returnedLength;
+		KdPrint((" buffer is samll \r\n"));
+		return  STATUS_BUFFER_OVERFLOW;
+
+	}
+
+	//   
+	// If we get here, the buffer IS going to be big enough for us, so   
+	// let's allocate some storage.   
+	//   
+	buffer = ExAllocatePoolWithTag(PagedPool, returnedLength, 'ipgD');
+
+	if (NULL == buffer) {
+
+		return  STATUS_INSUFFICIENT_RESOURCES;
+
+	}
+
+	//   
+	// Now lets go get the data   
+	//   
+	Status = ZwQueryInformationProcess(hProcess,
+		ProcessImageFileName,
+		buffer,
+		returnedLength,
+		&returnedLength);
+
+	if (NT_SUCCESS(Status)) {
+		//   
+		// Ah, we got what we needed   
+		//   
+		imageName = (PUNICODE_STRING)buffer;
+		//KdPrint(("------%S\n",imageName->Buffer));
+		RtlCopyUnicodeString(ProcessImagePath, imageName);
+
+	}
+
+	ZwClose(hProcess);
+
+	//   
+	// free our buffer   
+	//   
+	ExFreePool(buffer);
+
+	//   
+	// And tell the caller what happened.   
+	//      
+	return  Status;
+
 }
 
 VOID ProcessPre (
@@ -76,25 +141,56 @@ VOID ProcessPre (
 	UNREFERENCED_PARAMETER(Process);
 	UNREFERENCED_PARAMETER(ProcessId);
 	UNREFERENCED_PARAMETER(CreateInfo);
+	UNICODE_STRING ParentName;
+	UNICODE_STRING ProcessName;
+
 	if (NULL == CreateInfo)
 	{
-		KdPrint((" Process Exit\r\n"));
+		KdPrint((" gxb Process Exit\r\n"));
 		return;
 	}
 
+
+	ParentName.Length = 0;
+	ParentName.MaximumLength = 0;
+	ParentName.Buffer = NULL;
+
+	if (GetProcessImagePath(&ParentName, CreateInfo->ParentProcessId) == STATUS_BUFFER_OVERFLOW)
+	{
+		ParentName.Buffer = ExAllocatePoolWithTag(NonPagedPool, ParentName.Length,'me');
+		ParentName.MaximumLength = ParentName.Length;
+		GetProcessImagePath(&ParentName, CreateInfo->ParentProcessId);
+	}
+
+	ProcessName.Length = 0;
+	ProcessName.MaximumLength = 0;
+	ProcessName.Buffer = NULL;
+	if (GetProcessImagePath(&ProcessName, ProcessId) == STATUS_BUFFER_OVERFLOW)
+	{
+		ProcessName.Buffer = ExAllocatePoolWithTag(NonPagedPool, ProcessName.Length, 'nn');
+		ProcessName.MaximumLength = ProcessName.Length;
+		GetProcessImagePath(&ProcessName, ProcessId);
+	}
+
 	KdPrint(
-		("Create process ParentName:%S ProcessID:%S\n",
-			GetProcessImageFullPathByID(CreateInfo->ParentProcessId),
-			GetProcessImageFullPathByID(ProcessId)
-			) 
+		("gxb Create process \n gxbParentName:%S \n gxbProcessName:%S   \n",
+			ParentName.Buffer,
+			ProcessName.Buffer
+			)
 	);
-	if (wcswcs(GetProcessImageFullPathByID(ProcessId),L"notepad.exe") !=0)   // refuse notepad.exe
+
+	if (wcswcs(ProcessName.Buffer,L"SCBYDL") !=0)   // refuse notepad.exe
 	{
 		//修改返回结果为拒绝访问，使得创建进程失败
 		CreateInfo->CreationStatus = STATUS_ACCESS_DENIED;
+		KdPrint(("gxb refuse Process %S \r\n", ProcessName.Buffer));
 	}
+	ExFreePool(ParentName.Buffer);
+	ExFreePool(ProcessName.Buffer);
 	return;
 }
+
+
 VOID Unload(IN PDRIVER_OBJECT pDriverObject)
 {
 	UNREFERENCED_PARAMETER(pDriverObject);
@@ -103,11 +199,15 @@ VOID Unload(IN PDRIVER_OBJECT pDriverObject)
 }
 
 
+
+
+
 NTSTATUS DriverEntry(IN PDRIVER_OBJECT pDriverObject,IN PUNICODE_STRING pRegistryPath)
 {
 	UNREFERENCED_PARAMETER(pDriverObject);
 	UNREFERENCED_PARAMETER(pRegistryPath);
 
+	
 #ifdef _AMD64_
 	DbgPrint("64\r\n");
 	*((PCHAR)pDriverObject->DriverSection + 0x68) |= 0x20;
@@ -116,6 +216,9 @@ NTSTATUS DriverEntry(IN PDRIVER_OBJECT pDriverObject,IN PUNICODE_STRING pRegistr
 	*((PCHAR)pDriverObject->DriverSection + 0x34) |= 0x20;
 #endif // _AMD64_
 
+
+
+	////PsSetLoadImageNotifyRoutine
 	NTSTATUS re = STATUS_SUCCESS;
 	pDriverObject->DriverUnload = Unload;
 	//PsSetCreateProcessNotifyRoutine只能记录进程创建或消亡，不能阻止进程的创建，
--- 驱动接口函数/Get_Process_Info.c
+++ 驱动接口函数/Get_Process_Info.c
@@ -1,3 +1,16 @@
+Guid:
+    GetProcessImageNameByProcessID 通过进程ID获取进程的名字，限制16字节。 在32位系统下测试好用。
+	GetProcessImageFullPathByID   通过进程ID返回进程的全路径 函数在x32位系统下，好用，64位系统下不能返回在函数中申请的空间。  gxb 2018.6.5 
+	GetProcessImagePath   通过进程ID返回进程的全路径  在x32和x64位系统下全好用。gxb 2018.6.5 
+
+
+
+
+
+
+
+
+
 //declare section 
 NTKERNELAPI
 UCHAR * PsGetProcessImageFileName(__in PEPROCESS Process);
@@ -97,4 +110,138 @@ WCHAR* GetProcessImageFullPathByID(IN PHANDLE pID)
 		KdPrint(("ZwQueryInformationProcess error code %x ", status));
 
 	return ProcessImageName.Buffer;
+}
+
+///////////-------------------------//////////////////////////////
+NTSTATUS
+GetProcessImagePath(
+	OUT PUNICODE_STRING ProcessImagePath,
+	IN   HANDLE    dwProcessId
+)
+{
+	NTSTATUS Status;
+	HANDLE  hProcess;
+	PEPROCESS pEprocess;
+	ULONG  returnedLength;
+	ULONG  bufferLength;
+	PVOID  buffer;
+	PUNICODE_STRING imageName;
+
+
+	typedef NTSTATUS(*QUERY_INFO_PROCESS) (
+		__in HANDLE ProcessHandle,
+		__in PROCESSINFOCLASS ProcessInformationClass,
+		__out_bcount(ProcessInformationLength) PVOID ProcessInformation,
+		__in ULONG ProcessInformationLength,
+		__out_opt PULONG ReturnLength
+		);
+
+	QUERY_INFO_PROCESS ZwQueryInformationProcess =NULL;
+
+
+	PAGED_CODE();  // this eliminates the possibility of the IDLE Thread/Process   
+
+	if (NULL == ZwQueryInformationProcess) {
+
+		UNICODE_STRING routineName;
+
+		RtlInitUnicodeString(&routineName, L"ZwQueryInformationProcess");
+
+		ZwQueryInformationProcess =
+			(QUERY_INFO_PROCESS)MmGetSystemRoutineAddress(&routineName);
+
+		if (NULL == ZwQueryInformationProcess) {
+			DbgPrint("Cannot resolve ZwQueryInformationProcess/n");
+		}
+	}
+
+	Status = PsLookupProcessByProcessId((HANDLE)dwProcessId, &pEprocess);
+	if (!NT_SUCCESS(Status))
+		return Status;
+
+	Status = ObOpenObjectByPointer(pEprocess,           // Object   
+		OBJ_KERNEL_HANDLE,   // HandleAttributes   
+		NULL,                // PassedAccessState OPTIONAL   
+		GENERIC_READ,        // DesiredAccess   
+		*PsProcessType,      // ObjectType   
+		KernelMode,          // AccessMode   
+		&hProcess);
+	if (!NT_SUCCESS(Status))
+		return Status;
+
+
+	//   
+	// Step one - get the size we need   
+	//   
+	Status = ZwQueryInformationProcess(hProcess,
+		ProcessImageFileName,
+		NULL,  // buffer   
+		0,  // buffer size   
+		&returnedLength);
+
+
+	if (STATUS_INFO_LENGTH_MISMATCH != Status) {
+		KdPrint(("ZwQueryInformationProcess don't get buffer length\r\n"));
+		return  Status;
+
+	}
+
+	//   
+	// Is the passed-in buffer going to be big enough for us?    
+	// This function returns a single contguous buffer model...   
+	//   
+	bufferLength = returnedLength - sizeof(UNICODE_STRING);
+
+	if (ProcessImagePath->MaximumLength < bufferLength) {
+
+		ProcessImagePath->Length = (USHORT)returnedLength;
+		ProcessImagePath->MaximumLength = (USHORT)returnedLength;
+		KdPrint(("gxb buffer is samll \r\n"));
+		return  STATUS_BUFFER_OVERFLOW;
+
+	}
+
+	//   
+	// If we get here, the buffer IS going to be big enough for us, so   
+	// let's allocate some storage.   
+	//   
+	buffer = ExAllocatePoolWithTag(PagedPool, returnedLength, 'ipgD');
+
+	if (NULL == buffer) {
+
+		return  STATUS_INSUFFICIENT_RESOURCES;
+
+	}
+
+	//   
+	// Now lets go get the data   
+	//   
+	Status = ZwQueryInformationProcess(hProcess,
+		ProcessImageFileName,
+		buffer,
+		returnedLength,
+		&returnedLength);
+
+	if (NT_SUCCESS(Status)) {
+		//   
+		// Ah, we got what we needed   
+		//   
+		imageName = (PUNICODE_STRING)buffer;
+		KdPrint(("------%S\n",imageName->Buffer));
+		RtlCopyUnicodeString(ProcessImagePath, imageName);
+
+	}
+
+	ZwClose(hProcess);
+
+	//   
+	// free our buffer   
+	//   
+	ExFreePool(buffer);
+
+	//   
+	// And tell the caller what happened.   
+	//      
+	return  Status;
+
 }
